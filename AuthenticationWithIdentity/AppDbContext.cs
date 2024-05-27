eddf2a94461477ec61d1969004cb90aee4dcaa68
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-27 18:20:40 +0500

    Add project files.

---
+++ AuthenticationWithIdentity/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationWithIdentity
+{
+    public class AppDbContext : IdentityDbContext   
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
